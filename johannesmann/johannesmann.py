cf76badc99a4beb7af3408d75181b33492fd05ee
Leander Claes <claes@emt.uni-paderborn.de>
2024-04-28 16:30:48 +0200

    Implement real Johannesmann tessellation
    Previously the tessellation was just random lines. However lines are not
    supposed to cross. This is implemented now via binary space partitioning

--- johannesmann/johannesmann.py
+++ johannesmann/johannesmann.py
@@ -1,10 +1,12 @@
 """Main module."""
+from __future__ import annotations
+
 import numpy as np
 
 from dataclasses import dataclass
 from numpy.typing import NDArray
 
-__all__ = ['Line', 'Tessellation']
+__all__ = ['Line', 'Node', 'Tessellation']
 
 
 @dataclass
@@ -31,6 +33,20 @@ class Line:
         return y > self.y_0 + x * self.slope
 
 
+@dataclass
+class Node:
+    """Node for a binary tree to realise space partitioning.
+
+    Args:
+        line: Line object.
+        above: Node with lines in the space above the line in this node.
+        below: Node with lines in the space below the line in this node.
+    """
+    line: Line
+    above: Node = None
+    below: Node = None
+
+
 class Tessellation:
     """Class to realise the Johannesmann Spatial Tessellation on a given
     rectangular region.
@@ -48,7 +64,7 @@ class Tessellation:
         self.y_range = y_range
         self.cuts = cuts
 
-        self.lines = []
+        self.lines = None
 
         for _ in range(cuts):
             # Random point in range
@@ -56,10 +72,61 @@ class Tessellation:
             y = (np.random.rand() - 0.5) * self.y_range
             # Random slope
             slope = np.tan((np.random.rand() - 0.5) * np.pi)
-            self.lines.append(Line(
-                y_0=y - slope * x,
-                slope=slope
-            ))
+
+            if self.lines is None:
+                self.lines = Node(Line(
+                    y_0=y - slope * x,
+                    slope=slope
+                ))
+            else:
+                self.attach_node(
+                    self.lines, x, y,
+                    Line(y_0=y - slope * x, slope=slope)
+                )
+
+    def attach_node(self, node: Node, x: float, y: float, line: Line):
+        """Recursivity search the binary tree and attach a new node with a
+        line.
+
+        Args:
+            node: Node to check or search the children of.
+            x: X-Coordinate of the point defining origin of the new line.
+            y: Y-Coordinate of the point defining origin of the new line.
+            line: Line of the new node to attach to the tree.
+        """
+        if node.line.above(x, y):
+            if node.above is None:
+                node.above = Node(line)
+            else:
+                self.attach_node(node.above, x, y, line)
+        else:
+            if node.below is None:
+                node.below = Node(line)
+            else:
+                self.attach_node(node.below, x, y, line)
+
+    def search_tree(self, node: Node, x: float, y: float) -> list[bool]:
+        """Recursively search the tree to return a binary sequence which
+        identifies in which tile a given point is located.
+
+        Args:
+            node: Node to check or search the children of.
+            x: X-Coordinate of the point defining origin of the new line.
+            y: Y-Coordinate of the point defining origin of the new line.
+
+        Returns:
+            list: List of booleans that identify a tile.
+        """
+        if node.line.above(x, y):
+            if node.above is None:
+                return [True]
+            else:
+                return [True] + self.search_tree(node.above, x, y)
+        else:
+            if node.below is None:
+                return [False]
+            else:
+                return [False] + self.search_tree(node.below, x, y)
 
     def tile_id(self, x: float, y: float) -> int:
         """Return a unique id for the tile a point is placed on. This is done
@@ -73,8 +140,8 @@ class Tessellation:
         Returns:
             Id of the tile the point is placed on.
         """
-        aboves = [line.above(x, y) for line in self.lines]
-        return sum(2 ** exp * int(bit) for exp, bit in enumerate(aboves))
+        aboves = self.search_tree(self.lines, x, y)
+        return sum(int(2 ** exp * bit) for exp, bit in enumerate(aboves))
 
     def sample_2d(self, x_samples: int, y_samples: int,
                   squash_ids: bool = False) -> NDArray:
